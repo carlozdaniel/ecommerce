32d10ae915c880ab3df1c028c968e3efd4e206b9
carlos daniel <a20160383@gmail.com>
2020-09-11 14:49:42 -0500

    new products view

--- app/form_builders/bootstrap_form_builder.rb
+++ app/form_builders/bootstrap_form_builder.rb
@@ -3,16 +3,33 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
   [:text_field, :text_area, :url_field, :email_field, :namber_field].each do |metodo|
     define_method metodo do |name,*args|
       options = args.extract_options!
+      aditional_classes = "form-control floating-label"
         if options.has_key?(:class)
-          options[:class] += "form-control"
+          options[:class] += aditional_classes
         else
-          options[:class] = "form-control"
+          options[:class] = aditional_classes
         end
-      content_tag :div, class:"form-group" do
-        content_tag(:label,name,name: name) + super(name,options)
+        options[:pleceholder] = name.capitalize
+        content_tag :div, class:"form-group" do
+        super(name,options)
       end
     end
   end
+
+  def errors
+    if object.errors.any?
+      content_tag :div, class:"red large-padding white-text" do
+        content_tag(:h2, "error") +
+        content_tag(:ul) do
+          lis = ""
+          object.errors.full_messages.each do |err|
+            lis += content_tag :li, err
+          end
+          lis.html_safe
+        end
+    end
+  end
+end
 
   def submit(*args)
     content_tag :div do
